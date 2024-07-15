b24b0b16e2cf78098983322612645c4ec926ee7a
rayyanmoin12 <166857521+rayyanmoin12@users.noreply.github.com>
2024-07-15 20:35:22 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{
+    type: "number",
+    name: "firstNumber",
+    message: "Enter your first number"
+},
+{
+    type: "number",
+    name: "secondNumber",
+    message: "Enter your second number"
+},
+{
+    type: "list",
+    name: "operator",
+    message: "Choose an operation",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+},
+]);
+
+if (answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+} else {
+    console.log("Invalid operation");
+}
